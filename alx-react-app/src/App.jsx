95167e8b930504228e09c37c75fd350586bd075f
AbigiyaFikru <abigiya.fikru.walelegn@gmail.com>
2025-07-13 16:03:54 +0300

    Update App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -6,7 +6,7 @@ import WelcomeMessage from './components/WelcomeMessage';
 import Header from './components/Header';
 import MainContent from './components/MainContent';  // Fixed path
 import Footer from './components/Footer';
-
+import UserProfile from './components/UserProfile';
 function App() {
   const [count, setCount] = useState(0);
 
