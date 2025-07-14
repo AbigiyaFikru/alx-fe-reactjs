2e407933ea328e1021b2b7bae49d3503d5ac90fc
AbigiyaFikru <abigiya.fikru.walelegn@gmail.com>
2025-07-14 16:36:07 +0300

    Update Header.jsx

--- alx-react-app/src/components/Header.jsx
+++ alx-react-app/src/components/Header.jsx
@@ -1,7 +1,17 @@
 function Header() {
   return (
-    <header>
-      <h1>My Favorite Cities</h1>
+    <header style={{ 
+      backgroundColor: 'navy', 
+      color: 'white', 
+      textAlign: 'center',
+      padding: '20px',
+      marginBottom: '20px'
+    }}>
+      <h1 style={{
+        margin: 0,
+        fontSize: '2rem',
+        textShadow: '1px 1px 2px rgba(0,0,0,0.3)'
+      }}>My Favorite Cities</h1>
     </header>
   );
 }
