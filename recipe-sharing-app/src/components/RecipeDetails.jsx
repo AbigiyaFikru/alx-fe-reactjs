1b109eedf0783cbb263419f88d9115434a0a4c49
AbigiyaFikru <abigiya.fikru.walelegn@gmail.com>
2025-07-26 17:41:32 +0300

    Update RecipeDetails.jsx

--- recipe-sharing-app/src/components/RecipeDetails.jsx
+++ recipe-sharing-app/src/components/RecipeDetails.jsx
@@ -1,1 +1,24 @@
+import { useParams } from 'react-router-dom';
+import useRecipeStore from '../stores/recipeStore';
+import EditRecipeForm from './EditRecipeForm';
+import DeleteRecipeButton from './DeleteRecipeButton';
 
+const RecipeDetails = () => {
+  const { id } = useParams();
+  const recipe = useRecipeStore(state =>
+    state.recipes.find(r => r.id === Number(id))
+  );
+
+  if (!recipe) return <div>Recipe not found</div>;
+
+  return (
+    <div className="recipe-details">
+      <h1>{recipe.title}</h1>
+      <p>{recipe.description}</p>
+      <EditRecipeForm recipe={recipe} />
+      <DeleteRecipeButton recipeId={recipe.id} />
+    </div>
+  );
+};
+
+export default RecipeDetails;
