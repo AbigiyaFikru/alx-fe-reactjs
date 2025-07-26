9a7de4c40971b5bd68a3a05f4b5ff3dda9e8e40e
AbigiyaFikru <abigiya.fikru.walelegn@gmail.com>
2025-07-26 17:04:31 +0300

    Update RecipeList.jsx

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,1 +1,20 @@
+import useRecipeStore from '../stores/recipeStore'
 
+export default function RecipeList() {
+  const { recipes } = useRecipeStore()
+
+  return (
+    <div className="recipe-list">
+      {recipes.length === 0 ? (
+        <p>No recipes yet. Add one to get started!</p>
+      ) : (
+        recipes.map((recipe) => (
+          <div key={recipe.id} className="recipe-card">
+            <h3>{recipe.title}</h3>
+            <p>{recipe.description}</p>
+          </div>
+        ))
+      )}
+    </div>
+  )
+}
