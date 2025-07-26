a4d7f1aef5ff2d01dc81ade8c76c66a80293f89a
AbigiyaFikru <abigiya.fikru.walelegn@gmail.com>
2025-07-26 17:02:31 +0300

    Update recipeStore.js

--- recipe-sharing-app/src/stores/recipeStore.js
+++ recipe-sharing-app/src/stores/recipeStore.js
@@ -1,10 +1,17 @@
 import { create } from 'zustand'
 
 const useRecipeStore = create((set) => ({
-  recipes: [],
+  recipes: [
+    { id: 1, title: "Pasta Carbonara", description: "Classic Italian pasta with eggs, cheese, and pancetta" }
+  ],
   addRecipe: (newRecipe) => 
-    set((state) => ({ recipes: [...state.recipes, newRecipe] })),
-  setRecipes: (recipes) => set({ recipes })
+    set((state) => ({ 
+      recipes: [...state.recipes, { ...newRecipe, id: Date.now() }] 
+    })),
+  removeRecipe: (id) =>
+    set((state) => ({
+      recipes: state.recipes.filter(recipe => recipe.id !== id)
+    }))
 }))
 
 export default useRecipeStore
