ceac854ecf1aa5adf92dc110e369858b01ffbb06
AbigiyaFikru <abigiya.fikru.walelegn@gmail.com>
2025-08-12 20:28:56 +0300

    Update App.jsx

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -1,12 +1,11 @@
-export default function App() {
+import UserProfile from './components/UserProfile';
+
+function App() {
   return (
-    <div className="p-8 bg-blue-100 min-h-screen">
-      <h1 className="text-3xl font-bold text-blue-800">
-        Tailwind + React = Success! 🎉
-      </h1>
-      <p className="mt-4 text-gray-700">
-        If this text is styled, Tailwind is working!
-      </p>
+    <div className="app">
+      <UserProfile />
     </div>
   );
 }
+
+export default App;
