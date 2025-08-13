cb70da07adbaba05939305ad3e5724e099f27fa4
AbigiyaFikru <abigiya.fikru.walelegn@gmail.com>
2025-08-13 09:59:53 +0300

    Update App.jsx

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,10 +1,8 @@
 import SearchBar from './components/SearchBar';
-import './App.css';
 
 function App() {
   return (
-    <div className="app">
-      <h1>GitHub User Search</h1>
+    <div className="min-h-screen bg-gray-100 py-12 px-4 sm:px-6 lg:px-8">
       <SearchBar />
     </div>
   );
