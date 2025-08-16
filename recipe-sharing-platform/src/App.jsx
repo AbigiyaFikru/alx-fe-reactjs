eb6e1dc036573162f4c4294d02f2d9e0c48ddca3
AbigiyaFikru <abigiya.fikru.walelegn@gmail.com>
2025-08-16 18:27:49 +0300

    Update App.jsx

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,6 +1,7 @@
-import { Routes, Route } from 'react-router-dom'
-import HomePage from './components/HomePage'
-import RecipeDetail from './components/RecipeDetail'
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './components/HomePage';
+import RecipeDetail from './components/RecipeDetail';
+import NotFound from './components/NotFound'; // Optional 404 page
 
 function App() {
   return (
@@ -8,9 +9,10 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/recipe/:id" element={<RecipeDetail />} />
+        <Route path="*" element={<NotFound />} /> {/* Catch-all route */}
       </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
