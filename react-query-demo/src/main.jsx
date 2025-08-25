14e9f0008c89ebd0eb28c883252baf9dacf2e54b
AbigiyaFikru <abigiya.fikru.walelegn@gmail.com>
2025-08-25 15:44:19 +0300

    Update main.jsx

--- react-query-demo/src/main.jsx
+++ react-query-demo/src/main.jsx
@@ -1,27 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-import App from './App'
+import App from './App.jsx'
 import './index.css'
-
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      // show off caching behavior:
-      staleTime: 2 * 60 * 1000,     // 2 minutes "fresh" window
-      gcTime: 5 * 60 * 1000,        // cache kept for 5 minutes when unused (v5 rename of cacheTime)
-      refetchOnWindowFocus: true,
-    },
-  },
-})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-      {/* Devtools: open/close to inspect query cache */}
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
-  </React.StrictMode>
+    <App />
+  </React.StrictMode>,
 )
